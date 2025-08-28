d4ee01477234e4b9c8e8edef91d044eb3e12be0a
Jericho Wenzel Serrano <serranojerichowenzel@gmail.com>
2025-08-28 16:05:29 +0000

    Okay we actually prefer speed (startup phase) ! as this is a factor (our

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,2 @@
 import { config } from 'dotenv';
 config();
-
-import '@/ai/flows/summarize-message.ts';
-import '@/ai/flows/translate-message.ts';
-import '@/ai/flows/generate-welcome-message.ts';
-import '@/ai/flows/send-sms.ts';
